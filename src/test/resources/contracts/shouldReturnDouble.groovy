2560712a97a31815231b3d488e41018b91b31c30
poojakumari11228 <poojakumari11228@gmail.com>
2023-09-04 04:18:25 -0500

    Double a number contract

---
+++ src/test/resources/contracts/shouldReturnDouble.groovy
@@ -0,0 +1,18 @@
+package contracts
+
+import org.springframework.cloud.contract.spec.Contract
+Contract.make {
+    description "return double of a number"
+    request {
+        method GET()
+        url("/double") {
+            queryParameters {
+                parameter("number", "2")
+            }
+        }
+    }
+    response {
+        body("4")
+        status 200
+    }
+}
